7f4aa36aaae1ef3b27387d856ec90a7fd043e32e
Akash Kumar Shil <156079148+pixelsnail7@users.noreply.github.com>
2024-11-11 12:11:50 +0600

    Add files via upload

---
+++ src/arow.mjs
@@ -0,0 +1,31 @@
+// useState
+export function useState(initialState) {
+    let state = initialState;
+    
+    const setState = (newState) => {
+      state = newState;
+    };
+    
+    return [() => state, setState];
+}
+
+
+
+// useEffect
+export function useEffect(callback, dependencies) {
+    setInterval(
+        () => {
+            if (dependencies.length === 0) {
+                return callback();
+            }
+        
+            let prevDeps = useEffect.prevDeps || [];
+        
+            if (dependencies !== prevDeps) {
+                return callback();
+                useEffect.prevDeps = dependencies;
+            }
+        }, 99
+    )
+}
+
